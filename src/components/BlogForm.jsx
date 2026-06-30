491fd6f9fb5e3ef11fb332c4ef5a99f97e693e97
unknown <yanpieregc@gmail.com>
2026-06-30 17:49:09 -0500

    se agrego un hook personalizado para los input

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -1,36 +1,37 @@
-import { useState } from 'react'
+import { useField } from '../hooks/index.js'
 
 const BlogForm = ({ createBlog }) => {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
+  const title = useField('text')
+  const author = useField('text')
+  const url = useField('text')
 
   const clearInputs = () => {
-    setAuthor('')
-    setTitle('')
-    setUrl('')
+    title.reset()
+    author.reset()
+    url.reset()
   }
 
   const addBlog = (event) => {
     event.preventDefault()
 
     createBlog({
-      title: title,
-      author: author,
-      url: url
+      title: title.input.value,
+      author: author.input.value,
+      url: url.input.value
     })
 
-    clearInputs()
+    clearInputs
   }
 
   return (
     <>
       <h2>Add new blog</h2>
       <form onSubmit={ addBlog }>
-        <p>title <input id='title' type="text" value={title} onChange={({ target }) => setTitle(target.value)}/></p>
-        <p>author <input id='author' type="text" value={author} onChange={({ target }) => setAuthor(target.value)}/></p>
-        <p>url <input id='url' type="text" value={url} onChange={({ target }) => setUrl(target.value)}/></p>
+        <p>title <input id='title' {...title.input} /></p>
+        <p>author <input id='author' {...author.input} /></p>
+        <p>url <input id='url' {...url.input} /></p>
         <button type='submit'>create</button>
+        <button type='reset' onClick={clearInputs}>reset</button>
       </form>
     </>
   )
